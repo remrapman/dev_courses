d00d2cffc707cd3cb5c94d7fefd845fee373af96
Roman Boniuk <roma.boniuk@gmail.com>
2016-03-28 01:02:31 +0300

    HW 6 started, NOT COMPLETED

---
+++ home6_sorter/home6_sorter/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace home6_sorter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ home6_sorter/home6_sorter/Sorter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace home6_sorter
+{
+    class Sorter
+    {
+    private int[] SorterArray;
+
+        public Sorter (int[] array)
+    {
+        SorterArray = array;
+    }
+
+        public void Print()
+        {
+            for (int i = 0; i < SorterArray.Length; i++)
+            {
+                Console.WriteLine(SorterArray[i]);
+            }
+        }
+
+        public static int Swap(ref int c; ref int d)
+    {
+        int tmp = c;
+        c = d;
+        d = tmp;
+    }
+        public virtual void Sort(){}
+    }
+}
