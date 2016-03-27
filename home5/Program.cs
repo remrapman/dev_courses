102dbe648b3a45248973312e8b3ff894cf046948
Roman Boniuk <roma.boniuk@gmail.com>
2016-03-28 00:02:50 +0300

    First commit

---
+++ home5/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace home5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int[] myarray = new int[a];
+            string A = null;
+
+            Random rand = new Random();
+
+
+            for (int i = 0; i < a; i++)
+            {
+                myarray[i] = rand.Next(20);
+            }
+
+            Sorter Array = new Sorter(myarray);
+
+            Console.WriteLine("Select Sort Method");
+            Console.WriteLine("1 - Bubble Sort");
+            Console.WriteLine("2 - Insertion Sort");
+            Console.WriteLine("3 - Exit");
+            A = Console.ReadLine();
+            int SortMethod = Int32.Parse(A);
+
+            switch (SortMethod)
+            {
+
+                case 1:
+                    Console.WriteLine("Bubble Sort Selected");
+                    Console.WriteLine("++++++++++++Before+++++++++++++");
+                    Array.Print();
+                    Array.BubbleSort();
+                    Console.WriteLine("============After==============");
+                    Array.Print();
+                    break;
+
+                case 2:
+                    Console.WriteLine("Insertion Sort Selected");
+                    Console.WriteLine("++++++++++++Before+++++++++++++");
+                    Array.Print();
+                    Array.InsertionSort();
+                    Console.WriteLine("============After==============");
+                    Array.Print();
+                    break;
+
+                case 3:
+                    Console.WriteLine("You select EXIT. Good bye");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ home5/Sorter.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace home5
+{
+    class Sorter
+    {
+        private int[] SortArray;
+
+        public Sorter (int[] array)
+        {
+            SortArray = array;
+        }
+
+
+
+        public void Print()
+        {
+            for (int i = 0; i < SortArray.Length; i++)
+            {
+                Console.WriteLine(SortArray[i]);
+            }
+        }
+
+        public static void Swap(ref int c, ref int d)
+        {
+            int temp = c;
+            c = d;
+            d = temp;
+
+        }
+
+        public void BubbleSort()
+        {
+            bool wasSwap = true;
+            while (wasSwap == true)
+            {
+                wasSwap = false;
+                for (int i = 0; i < SortArray.Length - 1; i++)
+                {
+                    int b = i + 1;
+                    if (SortArray[i] > SortArray[b])
+                    {
+                        Swap(ref SortArray[i], ref SortArray[b]);
+                        wasSwap = true;
+                    }
+                }
+            }
+        }
+
+        public void InsertionSort()
+        {
+            for (int i = 0; i < SortArray.Length - 1; i++)
+            {
+                for (int j = i + 1; j > 0; j--)
+                {
+                    int b = j - 1;
+                    if (SortArray[b] > SortArray[j])
+                    {
+                        Swap(ref SortArray[b], ref SortArray[j]);
+                    }
+                }
+            }
+        }
+    }
+}
